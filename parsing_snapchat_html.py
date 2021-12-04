e31915913875fea86b6e7cac56eda396dd8e3849
Emre Cenk <emrecenk9@gmail.com>
2021-12-03 20:01:20 -0400

    craeted utility to parse through people and save messages in a dictionary with the keys as people

--- parsing_snapchat_html.py
+++ parsing_snapchat_html.py
@@ -1,20 +1,38 @@
 
+from bs4 import BeautifulSoup
+from collections import defaultdict
 
+class snap_html_parser(BeautifulSoup):
 
-class snap_html_parser:
-
-    def __init__(self, path_to_html: str):
+    def __init__(self, path_to_html: str, **kwargs):
         self.path = path_to_html
+        self.chat_text = self.get_html_as_text()
+        super().__init__(self.chat_text, "html.parser", **kwargs)
+
+
     def get_html_as_text(self) -> str:
         with open(self.path, "r", encoding="utf-8") as file:
             text = file.read()
         return text
-    def initialize_text(self):
-        self.chat_text = self.get_html_as_text()
 
+    def parse_people(self):
+        people = defaultdict(list)
+
+        current_person = ""
+        current_type = ""
+        current_date = ""
+        for tag in self.find_all("tr"):
+            info = tag.find_all("td")
+            if len(info) == 3:
+                current_person, current_type, current_date = info[0].text, info[1].text, info[2].text
+
+            elif len(info)>0:
+                print(info[0].text, current_person)
+                message = info[0].text
+                people[current_person].append(message)
+        print(people)
 if __name__ == '__main__':
     s = snap_html_parser("chat_history.html")
-    s.get_html_as_text()
-
+    s.parse_people()
 
 
